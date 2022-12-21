53a9af299df1c2ba63a98eb584912f8f87aed89b
Michael Rybka <merybka@gmail.com>
2022-12-21 18:46:42 +0000

    Add supported languages

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"github.com/library-development/go-schemacafe"
 )
 
-const usage = "Usage: schemac [go|ts] [dir]"
+const usage = "Usage: schemac [py|rb|java|cs|ts|go|rs] <dir>"
 
 func main() {
 	flag.Parse()
@@ -17,18 +17,31 @@ func main() {
 	if to == "" {
 		to = "."
 	}
+	var err error
 	switch lang {
-	case "go":
-		err := schemacafe.WriteGo(from, to)
-		if err != nil {
-			panic(err)
-		}
+	case "py":
+		err = schemacafe.WritePython(from, to)
+	case "rb":
+		err = schemacafe.WriteRuby(from, to)
+	case "java":
+		err = schemacafe.WriteJava(from, to)
+	case "cs":
+		err = schemacafe.WriteCSharp(from, to)
 	case "ts":
-		err := schemacafe.WriteTypescript(from, to)
-		if err != nil {
-			panic(err)
-		}
+		err = schemacafe.WriteTypescript(from, to)
+	case "go":
+		err = schemacafe.WriteGo(from, to)
+	case "rs":
+		err = schemacafe.WriteRust(from, to)
 	default:
 		fmt.Println(usage)
+	}
+	handleErr(err)
+}
+
+func handleErr(err error) {
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
 	}
 }
